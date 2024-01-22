f32f81becf933111a799f04b56d3b2e50332b685
Jacopo Guelpa <jacopo.guelpa@gmail.com>
2024-01-22 14:48:51 +0100

    modifiche

--- linkedin-clone/src/components/ModalProfile.jsx
+++ linkedin-clone/src/components/ModalProfile.jsx
@@ -22,47 +22,47 @@ const ModalProfile = () => {
         <Pencil className="fs-4" style={{ color: "#5e5e5e" }}></Pencil>
       </Button>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} className="text-secondary">
         <Modal.Header closeButton>
-          <Modal.Title>Modifica presentazione</Modal.Title>
+          <Modal.Title className="text-dark">Modifica presentazione</Modal.Title>
         </Modal.Header>
         <Modal.Body className="pt-1">
-          <p className="fs-6 mt-0 text-secondary">*Indica che è obbligatorio</p>
+          <p className="fs-6 mt-0 ">*Indica che è obbligatorio</p>
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label className="text-secondary">Nome*</Form.Label>
+              <Form.Label className="">Nome*</Form.Label>
               <Form.Control type="text" placeholder="" className="border border-dark" autoFocus required />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Cognome*</Form.Label>
+              <Form.Label className="">Cognome*</Form.Label>
               <Form.Control type="text" placeholder="" className="border border-dark" autoFocus required />
             </Form.Group>
             <Form.Group className="mb-4" controlId="exampleForm.ControlInput1">
-              <Form.Label>Nome aggiuntivo</Form.Label>
+              <Form.Label className="">Nome aggiuntivo</Form.Label>
               <Form.Control type="text" placeholder="" className="border border-dark" autoFocus />
             </Form.Group>
-            <p className="pt-2 mb-2">Pronuncia del nome</p>
-            <p>
+            <p className="pt-2 mb-2 ">Pronuncia del nome</p>
+            <p style={{ color: "#56687a" }}>
               <InfoSquareFill /> Può essere aggiunta solo usando la nostra app per dispositivi mobili
             </p>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Inserisci pronomi personalizzati</Form.Label>
+              <Form.Label className="">Inserisci pronomi personalizzati</Form.Label>
               <Form.Control type="text" placeholder="" className="border border-dark" autoFocus />
             </Form.Group>
-            <p>Indica i pronomi di genere che vuoi che gli altri usino per riferirsi a te.</p>
-            <p>
+            <p className="">Indica i pronomi di genere che vuoi che gli altri usino per riferirsi a te.</p>
+            <p className="">
               Scopri di più sui{" "}
               <a href="" style={{ textDecoration: "none", fontWeight: "bold", color: "#0a66c2" }}>
                 pronomi di genere.
               </a>
             </p>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Sommario*</Form.Label>
+              <Form.Label className="">Sommario*</Form.Label>
               <Form.Control type="text" placeholder="" className="border border-dark" autoFocus required />
             </Form.Group>
-            <h4>Posizione attuale</h4>
+            <h4 className="text-dark">Posizione attuale</h4>
             <Form.Group controlId="exampleForm.SelectCustom" className="mb-3">
-              <Form.Label>Posizione lavorativa*</Form.Label>
+              <Form.Label className="">Posizione lavorativa*</Form.Label>
               <Form.Select custom required className="border border-dark">
                 <option>Elemento 1</option>
                 <option>Elemento 2</option>
@@ -77,19 +77,19 @@ const ModalProfile = () => {
             </div>
             <Form.Check type="checkbox" id="custom-checkbox" label="Mostra l'azienda attuale nella mia presentazione" />
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Settore*</Form.Label>
+              <Form.Label className="">Settore*</Form.Label>
               <Form.Control type="text" placeholder="" autoFocus required />
             </Form.Group>
-            <p>
+            <p className="">
               Scopri di più sulle{" "}
               <a href="" style={{ textDecoration: "none", fontWeight: "bold" }}>
                 opzioni relative al settore
               </a>
             </p>
           </Form>
-          <h4>Formazione</h4>
+          <h4 className="text-dark">Formazione</h4>
           <Form.Group controlId="exampleForm.SelectCustom" className="mb-3">
-            <Form.Label>Formazione*</Form.Label>
+            <Form.Label className="">Formazione*</Form.Label>
             <Form.Select custom required>
               <option>Elemento 1</option>
               <option>Elemento 2</option>
@@ -104,30 +104,30 @@ const ModalProfile = () => {
           </div>
           <Form.Check type="checkbox" id="custom-checkbox" label="Mostra la formazione nella mia presentazione" />
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Settore*</Form.Label>
+            <Form.Label className="">Settore*</Form.Label>
             <Form.Control type="text" placeholder="" autoFocus required />
           </Form.Group>
-          <h4>Località</h4>
+          <h4 className="text-dark">Località</h4>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Paese/Area geografica*</Form.Label>
+            <Form.Label className="">Paese/Area geografica*</Form.Label>
             <Form.Control type="text" placeholder="" autoFocus required />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>CAP</Form.Label>
+            <Form.Label className="">CAP</Form.Label>
             <Form.Control type="text" placeholder="" autoFocus />
           </Form.Group>
           <Form.Group controlId="exampleForm.SelectCustom" className="mb-4">
-            <Form.Label>Città*</Form.Label>
+            <Form.Label className="">Città*</Form.Label>
             <Form.Select custom required>
               <option>Elemento 1</option>
               <option>Elemento 2</option>
               <option>Elemento 3</option>
             </Form.Select>
           </Form.Group>
-          <p className="fs-5">Informazioni di contatto</p>
-          <p>Aggiungi o modifica il tuo profilo URL, indirizzo email e altro</p>
+          <p className="fs-5 text-dark">Informazioni di contatto</p>
+          <p className="">Aggiungi o modifica il tuo profilo URL, indirizzo email e altro</p>
           <div style={{ fontWeight: "bold" }}>
-            <a href="" style={{ textDecoration: "none", backgroundColor: "#0a66c2" }}>
+            <a href="" style={{ textDecoration: "none", color: "#0a66c2" }}>
               Modifica le informazioni di contatto
             </a>
           </div>
