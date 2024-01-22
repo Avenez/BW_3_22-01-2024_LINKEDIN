d730a0ea8d45514a877a4713ccbe485b94730cb3
Avenez <145536778+Avenez@users.noreply.github.com>
2024-01-22 14:38:51 +0100

    profile input

--- linkedin-clone/src/components/Profile.jsx
+++ linkedin-clone/src/components/Profile.jsx
@@ -8,18 +8,21 @@ const Profile = () => {
       <Row className="profileContainer">
         <Col>
           <Row className="">
-            <div className="position-relative p-0 ">
-              <img
-                className="bgProfile"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu0lWaaWP-WmtxAAWkwJGAZFMyER0nyOUdsjF2d0aCap0mS1rO"
-                alt="profileBg"
-              />
-              <i className="bi bi-camera-fill text-primary photoBgIcon "></i>
+            <div className="position-relative p-0 cursor">
+              <input type="file" id="bgImage" hidden />
+              <label htmlFor="bgImage" className="bgProfile cursor">
+                <img
+                  className="bgProfile"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu0lWaaWP-WmtxAAWkwJGAZFMyER0nyOUdsjF2d0aCap0mS1rO"
+                  alt="profileBg"
+                />
+                <i className="bi bi-camera-fill text-primary photoBgIcon "></i>
+              </label>
               <div className="">
-                <input type="file" id="upload" hidden />
-                <label htmlFor="upload">
+                <input type="file" id="profileImage" hidden />
+                <label htmlFor="profileImage">
                   <img
-                    className="photoProfile"
+                    className="photoProfile cursor"
                     src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
                     alt="profilePicture"
                   />
@@ -44,8 +47,8 @@ const Profile = () => {
               <Col xs={12} md={8}>
                 <Row className="mt-4 text-start">
                   <Col className="d-md-flex ">
-                    <h2>ProfileName</h2>
-                    <button className="d-flex align-items-center outlineButtons px-2  rounded-5  fw-bold fw-bolder ms-2 py-0  ">
+                    <h2 className="me-2">ProfileName</h2>
+                    <button className="d-flex align-items-center outlineButtons px-2  rounded-5  fw-bold fw-bolder py-0  ">
                       <i class="bi bi-shield-check me-1"></i>
                       <p className="Verify mb-0 p-0  ">Inizia la verifica</p>
                     </button>
@@ -77,10 +80,10 @@ const Profile = () => {
               <Col></Col>
             </Row>
             <Row className="text-secondary mt-2 justify-content-start  ">
-              <Col xs={4} md={2} className="p-0 text-start me-1 ">
+              <Col xs={4} md={3} className="p-0 text-start me-1 ">
                 <button className="btn btn-primary rounded-4 fw-bold w-100">Disponibile per</button>
               </Col>
-              <Col xs={6} md={4} className="p-0  text-center me-1">
+              <Col xs={5} md={4} className="p-0  text-center me-1">
                 <button className="outlineButtons rounded-4 fw-bold w-100 ">Aggiungi sezione del profilo</button>
               </Col>
               <Col xs={2} md={1} className=" p-0 text-start">
