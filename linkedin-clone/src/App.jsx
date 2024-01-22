ef18a9e24834c905c5b3341af34d7f6421358274
Nat-Nigro <natalenigro15@gmail.com>
2024-01-22 13:12:15 +0100

    colonna destra

---
+++ linkedin-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Aside from "./components/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Aside />
+    </div>
+  );
+}
+
+export default App;
