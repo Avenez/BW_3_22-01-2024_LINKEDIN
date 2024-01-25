d194cfbe8b76c474f1beaeda5933ef8fc242634c
Nat-Nigro <natalenigro15@gmail.com>
2024-01-25 23:06:08 +0100

    ultimo commit

--- linkedin-clone/src/components/GeneratedPost.jsx
+++ linkedin-clone/src/components/GeneratedPost.jsx
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import post from "../redux/action";
+import post from "../redux/action/post";
 
 import { Row, Col } from "react-bootstrap";
+import { Chat, Command, HandThumbsUp, Plus, Send, Share } from "react-bootstrap-icons";
 
 const GeneratedPost = () => {
   let results = useSelector((state) => state.post.postState);
   console.log(results);
   let isLoading = useSelector((state) => state.post.isLoading);
   console.log(isLoading);
-  let resultsSlice = results.slice(0, 5);
+  let resultsSlice = results.slice(20, 28);
   console.log(resultsSlice);
   let dispatch = useDispatch();
   useEffect(() => {
@@ -26,12 +27,45 @@ const GeneratedPost = () => {
             </>
           ) : (
             resultsSlice.map((p) => (
-              <>
-                <img src={p.image} />
-                <p>{p.id}</p>
-              </>
+              <div className="bg-white mb-2 rounded border border-seondary-light">
+                <div className="d-flex">
+                  <img src={p.image} className="icon asideImg rounded-circle mx-2 my-1" />
+                  <div>
+                    <p className="fw-bold ms-1 mt-1">{p.username}</p>
+                    <p className="ms-1 text-secondary">{p.user.title}</p>
+                  </div>
+                  <p className="ms-auto me-2 mt-1 text-primary fw-bold ">Segui</p>
+                </div>
+                <p className="mx-2">{p.text}</p>
+                <hr />
+                <div className="d-flex justify-content-between mx-2 align-items-center text-secondary">
+                  <p className="commenti-home">
+                    <HandThumbsUp className="me-1 " />
+                    Consiglia
+                  </p>
+                  <p className="commenti-home">
+                    <Chat className="me-1" />
+                    Commenta
+                  </p>
+                  <p className="commenti-home">
+                    <Share className="me-1" />
+                    Diffondi post
+                  </p>
+                  <p className="commenti-home">
+                    <Send className="me-1" />
+                    Invia
+                  </p>
+                </div>
+              </div>
             ))
           )}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="my-4 text-center">
+            <span className="altri-post rounded-pill fw-bold">Vedi altri post</span>
+          </div>
         </Col>
       </Row>
     </>
@@ -39,75 +73,3 @@ const GeneratedPost = () => {
 };
 
 export default GeneratedPost;
-
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import post from "../redux/action";
-// import React from "react";
-// import { Row, Col } from "react-bootstrap";
-
-// const GeneratedPost = () => {
-//   let results = useSelector((state) => state.post.postState);
-//   let isLoading = useSelector((state) => state.post.isLoading);
-//   let dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(post());
-//   }, [dispatch]);
-
-//   return (
-//     <>
-//       <Row>
-//         <Col>
-//           {isLoading ? (
-//             <p>Caricamento in corso...</p>
-//           ) : (
-//             results.map((post, index) => (
-//               <React.Fragment key={post._id}>
-//                 <img src={post.user.image} alt={`Post di ${post.username}`} />
-//                 <p>{post.text}</p>
-//               </React.Fragment>
-//             ))
-//           )}
-//         </Col>
-//       </Row>
-//     </>
-//   );
-// };
-
-// export default GeneratedPost;
-
-// import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import post from "../redux/action";
-// import { Row, Col } from "react-bootstrap";
-
-// const GeneratedPost = () => {
-//   const dispatch = useDispatch();
-//   const results = useSelector((state) => state.post.postState);
-//   const isLoading = useSelector((state) => state.post.isLoading);
-
-//   useEffect(() => {
-//     dispatch(post());
-//   }, [dispatch]);
-
-//   return (
-//     <>
-//       <Row>
-//         <Col>
-//           {isLoading ? (
-//             <p>Caricamento in corso...</p>
-//           ) : (
-//             results.map((post) => (
-//               <div key={post._id}>
-//                 <p>{post.text}</p>
-//               </div>
-//             ))
-//           )}
-//         </Col>
-//       </Row>
-//     </>
-//   );
-// };
-
-// export default GeneratedPost;
