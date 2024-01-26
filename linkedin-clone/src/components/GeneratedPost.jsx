067a004e25d4a60dc033cb4b732b57d3e8f75162
Nat-Nigro <natalenigro15@gmail.com>
2024-01-26 10:57:27 +0100

    map

--- linkedin-clone/src/components/GeneratedPost.jsx
+++ linkedin-clone/src/components/GeneratedPost.jsx
@@ -1,46 +1,50 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import post from "../redux/action/post";
-
 import { Row, Col } from "react-bootstrap";
-import { Chat, Command, HandThumbsUp, Plus, Send, Share } from "react-bootstrap-icons";
+import { Chat, HandThumbsUp, Share, Send } from "react-bootstrap-icons";
 
 const GeneratedPost = () => {
-  let results = useSelector((state) => state.post.postState);
-  console.log(results);
-  let isLoading = useSelector((state) => state.post.isLoading);
-  console.log(isLoading);
-  let resultsSlice = results.slice(20, 28);
-  console.log(resultsSlice);
-  let dispatch = useDispatch();
+  const results = useSelector((state) => state.post.postState);
+  const isLoading = useSelector((state) => state.post.isLoading);
+  const [randomSlice, setRandomSlice] = useState({ start: 0, end: 8 });
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(post());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (results.length > 8) {
+      const maxStartIndex = results.length - 8;
+      const start = Math.floor(Math.random() * maxStartIndex);
+      setRandomSlice({ start, end: start + 8 });
+    }
+  }, [results.length]);
+
+  const resultsSlice = results.slice(randomSlice.start, randomSlice.end);
 
   return (
     <>
       <Row>
         <Col>
           {isLoading ? (
-            <>
-              <p>Caricamento in corso..</p>
-            </>
+            <p>Caricamento in corso..</p>
           ) : (
-            resultsSlice.map((p) => (
-              <div className="bg-white mb-2 rounded border border-seondary-light">
+            resultsSlice.map((p, index) => (
+              <div key={index} className="bg-white mb-2 rounded border border-secondary-light">
                 <div className="d-flex">
-                  <img src={p.image} className="icon asideImg rounded-circle mx-2 my-1" />
+                  <img src={p.image} alt="" className="icon asideImg rounded-circle mx-2 my-1" />
                   <div>
                     <p className="fw-bold ms-1 mt-1">{p.username}</p>
                     <p className="ms-1 text-secondary">{p.user.title}</p>
                   </div>
-                  <p className="ms-auto me-2 mt-1 text-primary fw-bold ">Segui</p>
+                  <p className="ms-auto me-2 mt-1 text-primary fw-bold">Segui</p>
                 </div>
                 <p className="mx-2">{p.text}</p>
                 <hr />
                 <div className="d-flex justify-content-between mx-2 align-items-center text-secondary">
                   <p className="commenti-home">
-                    <HandThumbsUp className="me-1 " />
+                    <HandThumbsUp className="me-1" />
                     Consiglia
                   </p>
                   <p className="commenti-home">
